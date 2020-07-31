9bd104755cdfd7d0301dc82aa5f482f683464928
twinh <twinhuang@qq.com>
2020-07-31 15:08:46 +0800

    refactor: 更新包名和目录，避免前后端冲突

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.1.0 (2020-07-30)
+
+
+### Features
+
+* 拆分出布局先关组件 ([](https://github.com/miaoxing/mxjs-layout/commit/))
---
+++ FooterBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class FooterBar extends React.Component {
+  render() {
+    return <div className="footer-bar footer-bar-fluid d-flex">
+      <style>
+        {`
+        body {
+            margin-bottom: ${this.props.height}px;
+        }
+        `}
+      </style>
+      {this.props.children}
+    </div>;
+  }
+}
+
+FooterBar.defaultProps = {
+  height: 58 // 48 + 10
+};
+
+export default FooterBar;
