5fd1161559a12eda6314a9f5801df79e80599a61
twinh <twinhuang@qq.com>
2021-04-30 21:42:22 +0800

    test(layout): 增加测试

---
+++ __tests__/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import {FooterBar} from '..';
+
+describe('test', () => {
+  test('basic', () => {
+    const result = render(<FooterBar/>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
+
